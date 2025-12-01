d7c00e2929207bb88b03594528dbfb51bca456ac
swan <swanmhallais@gmail.com>
2025-12-01 12:16:57 +0100

    Views

---
+++ librairie-back/src/main/java/g1/librairie_back/view/Views.java
@@ -0,0 +1,21 @@
+package g1.librairie_back.view;
+
+public class Views {
+
+public class Common {}  
+	
+	public class Compte extends Common {} 
+	
+	public class Admin extends Common {} 
+	
+	public class Client extends Common{} 
+	
+	public class Livre extends Common{}
+	
+	public class Article extends Common{}
+	
+	public class Papeterie extends Common{}
+	
+	public class Auteur extends Common{}
+	
+}
