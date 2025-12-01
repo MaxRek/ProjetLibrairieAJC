176ce2d945fe1ac32c4dd7822a00f58f339c107b
MaxRK <max.rekar@gmail.com>
2025-12-01 15:30:21 +0100

    Erreur dans tous les services en fait V2

--- librairie-back/src/main/java/g1/librairie_back/service/LivreService.java
+++ librairie-back/src/main/java/g1/librairie_back/service/LivreService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import g1.librairie_back.dao.IDAOArticle;
 import g1.librairie_back.model.Article;
