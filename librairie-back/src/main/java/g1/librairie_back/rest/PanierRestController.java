c32d601381a90b8a74fa809e11afe2e495986d9f
MaxRK <max.rekar@gmail.com>
2025-12-04 16:30:05 +0100

    Premier config back

--- librairie-back/src/main/java/g1/librairie_back/rest/PanierRestController.java
+++ librairie-back/src/main/java/g1/librairie_back/rest/PanierRestController.java
@@ -32,7 +32,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/panier")
-@CrossOrigin("*")
 public class PanierRestController {
 	private static final Logger log = LoggerFactory.getLogger(PanierRestController.class);
 
